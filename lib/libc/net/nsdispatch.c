a997441a281864c022760f764b41b8ffba5a3915
Root on xyz <root@xyz.csail.mit.edu>
2023-07-31 19:37:42 -0400

    NSSWITCH_ALTERNATE environment hack

--- lib/libc/net/nsdispatch.c
+++ lib/libc/net/nsdispatch.c
@@ -351,6 +351,9 @@ nss_configure(void)
 	path = getenv("NSSWITCH_CONF");
 	if (path == NULL)
 #endif
+	if (getenv("NSSWITCH_ALTERNATE") != NULL)
+		path = "/etc/nsswitch-alt.conf";
+	else
 		path = _PATH_NS_CONF;
 #ifndef NS_REREAD_CONF
 	/*
